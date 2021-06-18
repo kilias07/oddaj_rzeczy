250192c4db26d0a494e33a4029a4590e3ae7889e
Kamil Kiliasiński <68988039+kilias07@users.noreply.github.com>
2021-06-18 10:46:44 +0200

    add Section 3

--- src/components/sections/Section3.js
+++ src/components/sections/Section3.js
@@ -1,9 +1,18 @@
+import decoration from "../../assets/images/Decoration.svg";
+import sign from "../../assets/images/Signature.svg";
 const Section3 = () => {
   return (
-    <div style={{height: '100vh', border: "3px dashed black"}} id="section_3">
-      <h1>this is section 3</h1>
+    <div id="o_nas" className="about-us">
+      <div className="about-us__left">
+        <h1>O nas</h1>
+        <img src={decoration} alt="decoration" />
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque ipsum consequatur obcaecati quaerat, error saepe repellat.</p>
+        <img src={sign} alt="signature" />
+      </div>
+      <div className="about-us__right">
+      </div>
     </div>
   )
 }
 
-export default Section3
+export default Section3;
