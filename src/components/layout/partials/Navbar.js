d8ff7abf5a357726e9272a5ec4936daf816c7bcb
Kamil Kiliasiński <68988039+kilias07@users.noreply.github.com>
2021-06-18 10:45:27 +0200

    add Navbar

--- src/components/layout/partials/Navbar.js
+++ src/components/layout/partials/Navbar.js
@@ -1,61 +1,37 @@
-// import { NavHashLink} from 'react-router-hash-link';
-// const Navbar = () => {
-//   return (
-//     <nav>
-//       <h1>to jest navbar</h1>
-//       <ul>
-//         <li>
-//         <NavHashLink smooth to="/#section_1" activeClassName="selected" activeStyle={{color: "red"}} >
-//           link1
-//         </NavHashLink>
-//         </li>
-//         <li>
-//         <NavHashLink smooth to="/#section_2" activeClassName="selected" activeStyle={{color: "red"}}>
-//           link2
-//         </NavHashLink>
-//         </li>
-//         <li>
-//         <NavHashLink smooth to="/#section_3" activeClassName="selected" activeStyle={{color: "red"}}>
-//           link3
-//         </NavHashLink>
-//         </li>
-//       </ul>
-//     </nav>
-//   );
-// };
-
-// export default Navbar;
-
-
-
-import { Link, Element, Events, animateScroll as scroll, scrollSpy, scroller } from 'react-scroll';
-
+import { NavHashLink } from "react-router-hash-link";
 const Navbar = () => {
-  const onSetActiveHandler = () => {
-    
-  }
-
   return (
-    <nav style={{position: "fixed", height: '70px', border: '4px solid blue', width: '100%'}}>
-      <h1>to jest navbar</h1>
-      <ul>
-      <li>
-        <Link hashSpy spy activeClass="green" to="section_1" smooth duration={500}>
-          link1
-        </Link>
-        </li>
-        <li>
-        <Link hashSpy spy activeClass="green" to="section_2" smooth duration={500}>
-          link2
-        </Link>
-        </li>
-        <li>
-        <Link hashSpy spy activeClass="green" to="section_3" smooth duration={500}>
-          link3
-        </Link>
-        </li>
-      </ul>
-    </nav>
+    <div className="flex justify-end py-2 navbar__container">
+      <nav className="xl:w-2/5">
+        <ul className="flex justify-between">
+          <li>
+            <NavHashLink to="/" smooth>
+              Start
+            </NavHashLink>
+          </li>
+          <li>
+            <NavHashLink to="/#o_co_chodzi" smooth>
+              O co chodzi?
+            </NavHashLink>
+          </li>
+          <li>
+            <NavHashLink to="/#o_nas" smooth>
+              O nas
+            </NavHashLink>
+          </li>
+          <li>
+            <NavHashLink to="/#fundacje" smooth>
+              Fundacje i organizacje
+            </NavHashLink>
+          </li>
+          <li>
+            <NavHashLink to="/#kontakt" smooth>
+              Kontakt
+            </NavHashLink>
+          </li>
+        </ul>
+      </nav>
+    </div>
   );
 };
 
