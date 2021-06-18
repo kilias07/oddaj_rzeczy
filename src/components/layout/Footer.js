6373b301d908755974b6960a94d310896fc23d51
Kamil Kiliasiński <68988039+kilias07@users.noreply.github.com>
2021-06-18 10:47:07 +0200

    add Footer

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,7 +1,11 @@
+import logoInstagram from '../../assets/images/Instagram.svg';
+import logoFacebook from '../../assets/images/Facebook.svg';
 const Footer = () => {
   return (
     <footer>
-      <h1>To jest Footer</h1>
+      <h1>Copyright by Kamil</h1>
+      <img src={logoInstagram} alt="Logo Instagram" />
+      <img src={logoFacebook} alt="Logo Facebook" />
     </footer>
   )
 }
