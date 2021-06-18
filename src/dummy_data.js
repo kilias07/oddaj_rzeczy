2df68d7d35f523a77d654a6810fa0c70a4372228
Kamil Kiliasiński <68988039+kilias07@users.noreply.github.com>
2021-06-18 10:46:26 +0200

    add dummy temporary data

---
+++ src/dummy_data.js
@@ -0,0 +1,248 @@
+const database = [
+  {
+    title: "Fundacjom",
+    description:
+      "W naszej bazie znajdziesz listę zweryfikowanych Fundacji, z którymi współpracujemy. Możesz sprawdzić czym się zajmują, komu pomagają i czego potrzebują.",
+    posts: [
+      {
+        title: `Fundacji "Lorem ipsum1"`,
+        description: "lorem ipsum dasdasdasdsas dsa",
+        additional: "dasasd dasasdsad dasasd",
+      },
+      {
+        title: `Fundacji "Lorem ipsum2"`,
+        description: "lorem ipsum dasdasdasdsas dsa",
+        additional: "dasasd dasasdsad dasasd",
+      },
+      {
+        title: `Fundacji "Lorem ipsum3"`,
+        description: "lorem ipsum dasdasdasdsas dsa",
+        additional: "dasasd dasasdsad dasasd",
+      },
+      {
+        title: `Fundacji "Lorem ipsum4"`,
+        description: "lorem ipsum dasdasdasdsas dsa",
+        additional: "dasasd dasasdsad dasasd",
+      },
+      {
+        title: `Fundacji "Lorem ipsum5"`,
+        description: "lorem ipsum dasdasdasdsas dsa",
+        additional: "dasasd dasasdsad dasasd",
+      },
+      {
+        title: `Fundacji "Lorem ipsum6"`,
+        description: "lorem ipsum dasdasdasdsas dsa",
+        additional: "dasasd dasasdsad dasasd",
+      },
+    ],
+  },
+  {
+    title: "Organizacjom pozarządowym",
+    description:
+      "To jest opis do Organizacji pozarządowych dasdadsadasdsdsadsadsadasdasdasda lorem ipsum dsadasd asdas das dasdsa dFDSG HGHF JGJ TYJ FHJ",
+    posts: [
+      {
+        title: `Organizacja pozarządowa "Lorem ipsum1"`,
+        description: "loasdasdrem ipsum dasdasdasdsas dsa",
+        additional: "dasadssadasd dasasdsad dasasd",
+      },
+      {
+        title: `Organizacja pozarządowa "Lorem ipsum2"`,
+        description: "loasdasdrem ipsum dasdasdasdsas dsa",
+        additional: "dasadssadasd dasasdsad dasasd",
+      },
+      {
+        title: `Organizacja pozarządowa "Lorem ipsum3"`,
+        description: "loasdasdrem ipsum dasdasdasdsas dsa",
+        additional: "dasadssadasd dasasdsad dasasd",
+      },
+      {
+        title: `Organizacja pozarządowa "Lorem ipsum4"`,
+        description: "loasdasdrem ipsum dasdasdasdsas dsa",
+        additional: "dasadssadasd danhg dasasd",
+      },
+      {
+        title: `Organizacja pozarządowa "Lorem ipsum5"`,
+        description: "loasdasdrem ipsum dasdasdasdsas dsa",
+        additional: "dasadssadasd ghhgd dasauy",
+      },
+    ],
+  },
+  {
+    title: "Lokalnym zbiórkom",
+    description:
+      "To jest opis do Lokalnych zbiórek pozarządowych dasdasdasdasdasdasdasdasdasdasdasda fskdfsadf jldhos dfj;ash f;safhd ;jsdaf;h ",
+    posts: [
+      {
+        title: `Lokalnym zbiórkom "Lorem ipsum1"`,
+        description: "loasdasdrem ipsum dasdasdasdsas dsa",
+        additional: "dasadssadasd dasasdsad dasasd",
+      },
+      {
+        title: `Lokalnym zbiórkom "Lorem ipsum2"`,
+        description: "loasdasdrem ipsum dasdasdasdsas dsa",
+        additional: "dasadssadasd dasasdsad dasasd",
+      },
+      {
+        title: `Lokalnym zbiórkom "Lorem ipsum3"`,
+        description: "loasdasdrem ipsum dasdasdasdsas dsa",
+        additional: "dasadssadasd dasasdsad dasasd",
+      },
+      {
+        title: `Lokalnym zbiórkom "Lorem ipsum4"`,
+        description: "loasdasdrem ipsum dasdasdasdsas dsa",
+        additional: "dasadssadasd danhg dasasd",
+      },
+      {
+        title: `Lokalnym zbiórkom "Lorem ipsum5"`,
+        description: "loasdasdrem ipsum dasdasdasdsas dsa",
+        additional: "dasadssadasd ghhgd dasauy",
+      },
+      {
+        title: `Lokalnym zbiórkom "Lorem ipsum6"`,
+        description: "loasdasdrem ipsum dasdasdasdsas dsa",
+        additional: "dasadssadasd ghhgd dasauy",
+      },
+      {
+        title: `Lokalnym zbiórkom "Lorem ipsum7"`,
+        description: "loasdasdrem ipsum dasdasdasdsas dsa",
+        additional: "dasadssadasd ghhgd dasauy",
+      },
+      {
+        title: `Lokalnym zbiórkom "Lorem ipsum8"`,
+        description: "loasdasdrem ipsum dasdasdasdsas dsa",
+        additional: "dasadssadasd ghhgd dasauy",
+      },
+      {
+        title: `Lokalnym zbiórkom "Lorem ipsum9"`,
+        description: "loasdasdrem ipsum dasdasdasdsas dsa",
+        additional: "dasadssadasd ghhgd dasauy",
+      },
+    ],
+  },
+];
+
+export default database;
+
+// {
+//   "data": [
+//     {
+//       "title": "Fundacjom",
+//       "description": "To jest opis do Fundacji Losdadasdasdadasdasdasdasdasdasdasdds",
+//       "posts": [
+//         {
+//           "title": "Fundacji \"Lorem ipsum1\"",
+//           "description": "lorem ipsum dasdasdasdsas dsa",
+//           "additional": "dasasd dasasdsad dasasd"
+//         },
+//         {
+//           "title": "Fundacji \"Lorem ipsum2\"",
+//           "description": "lorem ipsum dasdasdasdsas dsa",
+//           "additional": "dasasd dasasdsad dasasd"
+//         },
+//         {
+//           "title": "Fundacji \"Lorem ipsum3\"",
+//           "description": "lorem ipsum dasdasdasdsas dsa",
+//           "additional": "dasasd dasasdsad dasasd"
+//         },
+//         {
+//           "title": "Fundacji \"Lorem ipsum4\"",
+//           "description": "lorem ipsum dasdasdasdsas dsa",
+//           "additional": "dasasd dasasdsad dasasd"
+//         },
+//         {
+//           "title": "Fundacji \"Lorem ipsum5\"",
+//           "description": "lorem ipsum dasdasdasdsas dsa",
+//           "additional": "dasasd dasasdsad dasasd"
+//         },
+//         {
+//           "title": "Fundacji \"Lorem ipsum6\"",
+//           "description": "lorem ipsum dasdasdasdsas dsa",
+//           "additional": "dasasd dasasdsad dasasd"
+//         }
+//       ]
+//     },
+//     {
+//       "title": "Organizacjom pozarządowym",
+//       "description": "To jest opis do Organizacji pozarządowych dasdadsadasdsdsadsadsadasdasdasda",
+//       "posts": [
+//         {
+//           "title": "Organizacja pozarządowa \"Lorem ipsum1\"",
+//           "description": "loasdasdrem ipsum dasdasdasdsas dsa",
+//           "additional": "dasadssadasd dasasdsad dasasd"
+//         },
+//         {
+//           "title": "Organizacja pozarządowa \"Lorem ipsum2\"",
+//           "description": "loasdasdrem ipsum dasdasdasdsas dsa",
+//           "additional": "dasadssadasd dasasdsad dasasd"
+//         },
+//         {
+//           "title": "Organizacja pozarządowa \"Lorem ipsum3\"",
+//           "description": "loasdasdrem ipsum dasdasdasdsas dsa",
+//           "additional": "dasadssadasd dasasdsad dasasd"
+//         },
+//         {
+//           "title": "Organizacja pozarządowa \"Lorem ipsum4\"",
+//           "description": "loasdasdrem ipsum dasdasdasdsas dsa",
+//           "additional": "dasadssadasd danhg dasasd"
+//         },
+//         {
+//           "title": "Organizacja pozarządowa \"Lorem ipsum5\"",
+//           "description": "loasdasdrem ipsum dasdasdasdsas dsa",
+//           "additional": "dasadssadasd ghhgd dasauy"
+//         }
+//       ]
+//     },
+//     {
+//       "title": "Lokalnym zbiórkom",
+//       "description": "To jest opis do Lokalnych zbiórek pozarządowych dasdasdasdasdasdasdasdasdasdasdasda",
+//       "posts": [
+//         {
+//           "title": "Lokalnym zbiórkom \"Lorem ipsum1\"",
+//           "description": "loasdasdrem ipsum dasdasdasdsas dsa",
+//           "additional": "dasadssadasd dasasdsad dasasd"
+//         },
+//         {
+//           "title": "Lokalnym zbiórkom \"Lorem ipsum2\"",
+//           "description": "loasdasdrem ipsum dasdasdasdsas dsa",
+//           "additional": "dasadssadasd dasasdsad dasasd"
+//         },
+//         {
+//           "title": "Lokalnym zbiórkom \"Lorem ipsum3\"",
+//           "description": "loasdasdrem ipsum dasdasdasdsas dsa",
+//           "additional": "dasadssadasd dasasdsad dasasd"
+//         },
+//         {
+//           "title": "Lokalnym zbiórkom \"Lorem ipsum4\"",
+//           "description": "loasdasdrem ipsum dasdasdasdsas dsa",
+//           "additional": "dasadssadasd danhg dasasd"
+//         },
+//         {
+//           "title": "Lokalnym zbiórkom \"Lorem ipsum5\"",
+//           "description": "loasdasdrem ipsum dasdasdasdsas dsa",
+//           "additional": "dasadssadasd ghhgd dasauy"
+//         },
+//         {
+//           "title": "Lokalnym zbiórkom \"Lorem ipsum6\"",
+//           "description": "loasdasdrem ipsum dasdasdasdsas dsa",
+//           "additional": "dasadssadasd ghhgd dasauy"
+//         },
+//         {
+//           "title": "Lokalnym zbiórkom \"Lorem ipsum7\"",
+//           "description": "loasdasdrem ipsum dasdasdasdsas dsa",
+//           "additional": "dasadssadasd ghhgd dasauy"
+//         },
+//         {
+//           "title": "Lokalnym zbiórkom \"Lorem ipsum8\"",
+//           "description": "loasdasdrem ipsum dasdasdasdsas dsa",
+//           "additional": "dasadssadasd ghhgd dasauy"
+//         },
+//         {
+//           "title": "Lokalnym zbiórkom \"Lorem ipsum9\"",
+//           "description": "loasdasdrem ipsum dasdasdasdsas dsa",
+//           "additional": "dasadssadasd ghhgd dasauy"
+//         }
+//       ]
+//     }
+//   ]
+// }
