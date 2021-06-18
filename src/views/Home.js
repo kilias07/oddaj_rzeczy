02f14cbb37368d7aec3ad7b7dd533339e377e1a2
Kamil Kiliasiński <68988039+kilias07@users.noreply.github.com>
2021-06-18 10:47:15 +0200

    add HomePage

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,14 +1,18 @@
 import Section1 from "../components/sections/Section1";
 import Section2 from "../components/sections/Section2";
 import Section3 from "../components/sections/Section3";
+import Section4 from "../components/sections/Section4";
+import Section5 from "../components/sections/Section5";
 
 const Home = () => {
   return (
-    <>
+    <div>
       <Section1 />
       <Section2 />
       <Section3 />
-    </>
+      <Section4 />
+      <Section5 />
+    </div>
   );
 };
 
